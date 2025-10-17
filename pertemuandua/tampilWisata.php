5b70ec21650bc3d5b7b1857e2ea7563d31394525
BintangAraka07 <bintangaraka11@gmail.com>
2025-10-17 20:34:24 +0700

    pesan perubahan

--- pertemuandua/tampilWisata.php
+++ pertemuandua/tampilWisata.php
@@ -10,7 +10,7 @@ function curl($url){
 }
 
 // Ambil data JSON dari getWisata.php
-$send = curl("http://localhost/rekayasaweb/json/getWisata.php");
+$send = curl("http://localhost/rekayasaweb/pertemuandua/getWisata.php");
 
 // Ubah JSON menjadi array PHP
 $data = json_decode($send, TRUE);
