8374f7d86d73581e0245bd2c566c88a947873674
BintangAraka07 <bintangaraka11@gmail.com>
2025-12-16 11:36:23 +0700

    pesan perubahan

--- proyekakhir/routes/api.php
+++ proyekakhir/routes/api.php
@@ -6,23 +6,31 @@ use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\PelangganController;
 
+// Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Middleware auth
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::post('/produk/create', [ProdukController::class, 'create']);
-    Route::get('/produk/read', [ProdukController::class, 'read']);
-    Route::put('/produk/update', [ProdukController::class, 'update']);
-    Route::delete('/produk/delete', [ProdukController::class, 'delete']);
+    // Produk
+    Route::post('/produk', [ProdukController::class, 'create']);
+    Route::get('/produk', [ProdukController::class, 'read']);
+    Route::get('/produk/{id}', [ProdukController::class, 'show']); // optional single produk
+    Route::put('/produk/{id}', [ProdukController::class, 'update']);
+    Route::delete('/produk/{id}', [ProdukController::class, 'delete']);
 
-    Route::post('/kategori/create', [KategoriController::class, 'create']);
-    Route::get('/kategori/read', [KategoriController::class, 'read']);
-    Route::put('/kategori/update', [KategoriController::class, 'update']);
-    Route::delete('/kategori/delete', [KategoriController::class, 'delete']);
+    // Kategori
+    Route::post('/kategori', [KategoriController::class, 'create']);
+    Route::get('/kategori', [KategoriController::class, 'read']);
+    Route::get('/kategori/{id}', [KategoriController::class, 'show']); // optional single kategori
+    Route::put('/kategori/{id}', [KategoriController::class, 'update']);
+    Route::delete('/kategori/{id}', [KategoriController::class, 'delete']);
 
-    Route::post('/pelanggan/create', [PelangganController::class, 'create']);
-    Route::get('/pelanggan/read', [PelangganController::class, 'read']);
-    Route::put('/pelanggan/update', [PelangganController::class, 'update']);
-    Route::delete('/pelanggan/delete', [PelangganController::class, 'delete']);
+    // Pelanggan
+    Route::post('/pelanggan', [PelangganController::class, 'create']);
+    Route::get('/pelanggan', [PelangganController::class, 'read']);
+    Route::get('/pelanggan/{id}', [PelangganController::class, 'show']); // optional single pelanggan
+    Route::put('/pelanggan/{id}', [PelangganController::class, 'update']);
+    Route::delete('/pelanggan/{id}', [PelangganController::class, 'delete']);
 });
